e89ff6d86745c3fb7d69cb588fd9216aea1803a8
luizoe <luizoe@users.noreply.github.com>
2015-11-13 14:51:13 -0200

    ► Fix removed special characters in feature and in annotation ► Fixed for Java 7(pom.xml)

--- bddEx/src/test/java/feature/clinica/ExameRotinaSteps.java
+++ bddEx/src/test/java/feature/clinica/ExameRotinaSteps.java
@@ -20,22 +20,22 @@ public class ExameRotinaSteps {
 		dave = new Cliente(nomeTraci,new Cat(nomeGato),new Cat(nomeGato2));
 	}
 	
-	@Then("'(.+)' realiza as '(.+)' (que tem uma taxa normal de '(\\d+)' reais). ")
+	@Then("'(.+)' realiza as '(.+)' que tem uma taxa normal de '(\\d+)' reais")
 	public void cobrarCastra(final String nomeVet,final String servico, final double valor){
 		vet = new Veterinario(nomeVet);
                 rec = new Recibo(dave, vet);
                 rec.adicionaServico(servico, valor);
 	}
         
-        @Then("Traci pega seus gatos naquela noite, mas deseja pagar pelo servico ao final do mes.")
+        @Then("Traci pega seus gatos naquela noite, mas deseja pagar pelo servico ao final do mes")
 	public void pegaGatos(){
 	}
         
-        @Then("Naquele dia, ela recebe uma única fatura com os procedimentos detalhadas.")
+        @Then("Naquele dia, ela recebe uma unica fatura com os procedimentos detalhadas")
 	public void recebeFatura(){
 	}
 	
-	@Then("Dave paga em dinheiro em um valor de '(\\d+)' antes de ele sair para o trabalho e lhe e fornecido um recibo de pagamento para os servicos.")
+	@Then("No final do mes, ela recebe um boleto referente aquela conta, com o qual ela paga pelo servico, entao ela pagou com '(\\d+)' reais a castracao")
 	public void pagaEmDinheiroERecebeRecibo(final double total){
                 //Assert.assertTrue(rec.getTotal().equals(rec.getTotal()));
                 System.out.println(rec.retornaRecibo());
--- bddEx/src/test/resources/feature/clinica/exame_rotina.feature
+++ bddEx/src/test/resources/feature/clinica/exame_rotina.feature
@@ -2,8 +2,8 @@ Feature: Exercicio aula
 	To: Ganhar nota
 	
 	Scenario: Cetro Universitario - Catolica  de SC
-            Given 'Traci Heinrich' traz seus dois gatos, 'Tweedle Dee' e 'Tweedle Dum', para serem castrados. 
-            Then 'Dr. Roberts' realiza as 'esterilizacoes' (que tem uma taxa normal de '60' reais). 
+            Given 'Traci Heinrich' traz seus dois gatos, 'Tweedle Dee' e 'Tweedle Dum', para serem castrados.
+            Then 'Dr. Roberts' realiza as 'esterilizacoes' que tem uma taxa normal de '60' reais.
             Then Traci pega seus gatos naquela noite, mas deseja pagar pelo servico ao final do mes. 
-            Then Naquele dia, ela recebe uma única fatura com os procedimentos detalhadas. 
+            Then Naquele dia, ela recebe uma unica fatura com os procedimentos detalhadas. 
             Then No final do mes, ela recebe um boleto referente aquela conta, com o qual ela paga pelo servico, entao ela pagou com '100' reais a castracao.
